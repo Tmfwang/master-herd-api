f954035d7050407cdf3e6dbf09d6cf04406d39f0
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-02-01 19:24:52 +0100

    debug true i prod

--- fordypningsprosjektAPI/prod_settings.py
+++ fordypningsprosjektAPI/prod_settings.py
@@ -2,7 +2,7 @@ from fordypningsprosjektAPI.base_settings import *
 
 # Override base.py settings here
 
-DEBUG = False
+DEBUG = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
