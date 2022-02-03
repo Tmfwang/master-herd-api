5a94b1a4d27fc7978c29dddb1a77dd737b124419
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-02-03 12:44:07 +0100

    test 3

--- supervision/views.py
+++ supervision/views.py
@@ -126,8 +126,8 @@ class GetSupervisions(APIView):
     for supervision in users_supervisions:
       supervisions_response.append(format_supervision(supervision))
       
-    # return HttpResponse(json.dumps(supervisions_response), status=status.HTTP_200_OK, content_type='application/json')
-    return Response(data=json.dumps(supervisions_response), status=status.HTTP_200_OK)
+    return HttpResponse(json.dumps(supervisions_response), status=status.HTTP_200_OK, content_type='application/json')
+    #return Response(data=json.dumps(supervisions_response), status=status.HTTP_200_OK, content_type='application/json')
 
   # Creates a supervision database object (with accompanying observations), that is set to belong to the user that performs the request.
   def post (self, request, format=json):      
