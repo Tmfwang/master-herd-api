f46db705cf7754840d012b756dc539e72ff296d0
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-02-03 13:45:39 +0100

    Test 8

--- fordypningsprosjektAPI/prod_settings.py
+++ fordypningsprosjektAPI/prod_settings.py
@@ -5,6 +5,7 @@ from fordypningsprosjektAPI.base_settings import *
 DEBUG = False
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
