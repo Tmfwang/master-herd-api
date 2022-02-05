b2c4c1cd335b9de0974bdf0fe36578b06aada693
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-02-05 17:13:10 +0100

    La til feilmelding ved registrering av bruker med epost som allerede er brukt

--- authentication/views.py
+++ authentication/views.py
@@ -23,12 +23,15 @@ class ListUsers(APIView):
 
 
     def post (self, request, format=json):
-      print(request.data)
       
       if(request.data.get("password1") != request.data.get("password2") or not request.data.get("password1")):
         return HttpResponse('{"error": "Passordene stemmer ikke overens"}', status=status.HTTP_400_BAD_REQUEST)
         # return Response(data='{"error": "Passordene stemmer ikke overens"}', status=status.HTTP_400_BAD_REQUEST)
-           
+
+
+      if(User.objects.filter(email=request.data.get("email").lower()).count() > 0):
+        return HttpResponse('{"error": "Denne e-post-adressen har allerede blitt brukt"}', status=status.HTTP_400_BAD_REQUEST)
+        
       new_user = User.objects.create(email=request.data.get("email").lower(), password=request.data.get("password1"), full_name=request.data.get("full_name"), gaards_number=request.data.get("gaards_number"), bruks_number=request.data.get("bruks_number"), municipality=request.data.get("municipality"))
 
       if(new_user):
