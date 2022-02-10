b611be3ff7d6619cc1ce3dd34b2136ff7dc4aeee
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-02-10 15:41:42 +0100

    Endret cors

--- fordypningsprosjektAPI/prod_settings.py
+++ fordypningsprosjektAPI/prod_settings.py
@@ -4,8 +4,11 @@ from fordypningsprosjektAPI.base_settings import *
 
 DEBUG = False
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",
+]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
